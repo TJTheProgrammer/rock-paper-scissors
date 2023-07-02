5bc661b02b7a2dbcab1f6eda85f8bd74df56cb32
TJTheProgrammer <57811269+TJTheProgrammer@users.noreply.github.com>
2023-07-02 16:51:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+
+var player_choice = ""
+var player_points = 0
+
+var NPC_choice = ""
+var NPC_points = 0
+
+var selection = 0
+
+
+
+
+
+
+rock.addEventListener("click", e => {
+    player_choice = "Rock"
+    selection = Math.floor(Math.random() * 3)
+    if (selection == 0) {
+        NPC_choice = "Rock"
+    }
+    if (selection == 1) {
+        NPC_choice = "Paper"
+        NPC_points = NPC_points + 1
+    }
+    if (selection == 2) {
+        NPC_choice = "Scissors"
+        player_points = player_points + 1
+    }
+    document.getElementById("score").innerHTML = player_points+":"+NPC_points
+    document.getElementById("message").innerHTML = "You chose "+player_choice+", they chose " + NPC_choice 
+
+}
+    ) 
+
+
+paper.addEventListener("click", e => {
+    player_choice = "Paper"
+    selection = Math.floor(Math.random() * 3)
+    if (selection == 0) {
+        NPC_choice = "Rock"
+        player_points = player_points+1
+    }
+    if (selection == 1) {
+        NPC_choice = "Paper"
+    }
+    if (selection == 2) {
+        NPC_choice = "Scissors"
+        NPC_points = NPC_points + 1
+    }
+    document.getElementById("score").innerHTML = player_points+":"+NPC_points
+    document.getElementById("message").innerHTML = "You chose "+player_choice+", they chose " + NPC_choice 
+
+}
+    ) 
+
+scissors.addEventListener("click", e => {
+    player_choice = "Scissors"
+    selection = Math.floor(Math.random() * 3)
+    if (selection == 0) {
+        NPC_choice = "Rock"
+        NPC_points = NPC_points + 1
+    }
+    if (selection == 1) {
+        NPC_choice = "Paper"
+        player_points = player_points + 1
+    }
+    if (selection == 2) {
+        NPC_choice = "Scissors"
+    }
+    document.getElementById("score").innerHTML = player_points+":"+NPC_points
+    document.getElementById("message").innerHTML = "You chose "+player_choice+", they chose " + NPC_choice 
+
+    
+    }
+        ) 
